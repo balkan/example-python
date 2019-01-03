70c51d0d227c54df5978d638a209b039b073795b
Muhammet Alkan <burak.alkan@MuhammetAlkan.local>
2019-01-03 14:57:54 +0400

    increase test cove

--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(awesome.smile(), ":)")
         self.assertEqual(awesome.frown(), ":(")
         self.assertEqual(awesome.some_other_func(), ":/")
+        self.assertEqual(awesome.yet_other_func(), ":/")
 
 
 if __name__ == '__main__':
